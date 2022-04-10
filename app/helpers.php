fea61bfdf806cc82b2030df54c98123915ff3342
rivaiarvian <rivai.arviannn@gmail.com>
2022-04-10 08:19:18 +0700

    create helpers

---
+++ app/helpers.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Http;
+
+function getUser($userId){
+    $url = env('URL_SERVICE_USER').'users/'.$userId;
+
+    try {
+       $response = Http::timeout(10)->get($url);
+       $data = $response->json();
+       $data['http_code'] = $response->getStatusCode();
+       return $data;
+    } catch (\Throwable $e) {
+        return [
+            'satatus' =>'error',
+            'http_code' =>500,
+            'message' => 'Service user unavailable'
+        ]
+    }
+}
+
+function getUserById($userId=[]){
+    $url = env('URL_SERVICE_USER').'users/'.$userId;
+
+    try {
+        if(count($userId) === 0){
+            return [
+                'satatus' =>'success',
+                'http_code' =>200,
+                'data' => []
+            ]
+        }
+
+        $response = Http::timeout(10)->get($url,['user_id[]'=>$userId]);
+        $data = $response->json();
+        $data['http_code'] = $response->getStatusCode();
+        return $data;
+    } catch (\Throwable $e) {
+        return [
+            'satatus' =>'error',
+            'http_code' =>500,
+            'message' => 'Service user unavailable'
+        ]
+    }
+}
